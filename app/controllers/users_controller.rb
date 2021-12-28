c992eb5d67c346d136dacaed8499c4f1d51f5ac9
chiku3 <p314chiku.y@gmail.com>
2021-12-28 07:48:36 +0000

    12/28-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new
+    @user.save
+    redirect_to users_path(@user)
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :body, :profile_image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:"homes#top"
+  get "home/about" => "homes#about"
   
-  resource :users,only:[:show]
+  resources :users, only:[:show, :create, :edit, :update]
+  resources :books,only:[:new, :create, :index, :show, :destroy]
 
 end
